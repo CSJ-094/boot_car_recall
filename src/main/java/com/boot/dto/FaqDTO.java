2b837bba52378c5b900435073a035ff7489b4f25
yeseong1108 <ingyoeba1108@naver.com>
2025-11-05 10:20:18 +0900

    gd

--- src/main/java/com/boot/dto/FaqDTO.java
+++ src/main/java/com/boot/dto/FaqDTO.java
@@ -1,6 +1,5 @@
 package com.boot.dto;
 
-<<<<<<< HEAD
 import java.sql.Timestamp;
 
 import lombok.AllArgsConstructor;
@@ -17,15 +16,4 @@ public class FaqDTO {
     private Timestamp faqDate;
     private String faqDate2; // 화면 표시용 날짜 (예: 2025-11-04 11:49)
     private String faqCategory; // 카테고리
-=======
-import lombok.Data;
-
-@Data
-public class FaqDTO {
-    private long faq_id;
-    private String category;
-    private String question;
-    private String answer;
-    private String created_at;
->>>>>>> ea43a775c9b39041c2c846933da45c1acbc879e0
 }
